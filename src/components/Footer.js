736ce74c1fdc780c1fbbb82a4eaf362a1d71b9db
Jimmy McBroom <james.mcbroom@gmail.com>
2018-02-12 12:01:25 -0500

    title & link is good enough

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Footer = (source) => (
   <div className="tl pa2">
-    <span className="db f5 fw7 bt tr"><a href="https://cityofdetroit.github.io/iet">Built by IET</a> using <a href={source}>open data</a></span>
+    <span className="db f5 fw7 bt tr"><a href="https://cityofdetroit.github.io/iet">
+    Built by IET</a> using <a href='https://data.detroitmi.gov/d/dxgi-9s8s'>open data</a></span>
   </div>
 )
 
