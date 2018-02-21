a4cdaf36251fe2970294bca4e817ce5c792d68d5
Jimmy McBroom <james.mcbroom@gmail.com>
2018-02-21 09:51:38 -0500

    add google analytics

--- src/index.js
+++ src/index.js
@@ -6,12 +6,37 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-107915075-6');
+
+class GAListener extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentDidMount() {
+    this.sendPageView(this.context.router.history.location);
+    this.context.router.history.listen(this.sendPageView);
+  }
+
+  sendPageView(location) {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
 ReactDOM.render(
-  <Router basename="/parcel-viewer">
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/:name' component={App} />
-    </Switch>
-  </Router>,
+  <GAListener>
+    <Router basename="/parcel-viewer">
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/:name' component={App} />
+      </Switch>
+    </Router>
+  </GAListener>,
   document.getElementById('root'));
 registerServiceWorker();
