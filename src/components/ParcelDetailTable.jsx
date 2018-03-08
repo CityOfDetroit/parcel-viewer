fd1cbd3d9654ffd47d278fc87e82393fe1bf2c1d
jessica <jessica.mcinchak@gmail.com>
2018-03-08 10:53:21 -0500

    add extra parcel details

--- src/components/ParcelDetailTable.jsx
+++ src/components/ParcelDetailTable.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
 import moment from 'moment'
 import _ from 'lodash'
 
 class ParcelDetailTable extends React.Component {
-
   render() {
     const parcel = this.props.parcel
 
@@ -22,26 +20,31 @@ class ParcelDetailTable extends React.Component {
       'Owner Address': `${parcel.owner_street}, ${parcel.owner_city}, ${parcel.owner_state}`,
       'Last Sale Date': parcel.last_sale_date ? moment(parcel.last_sale_date).format('LL') : `No record`,
       'Last Sale Price': `$${_.parseInt(parcel.last_sale_price).toLocaleString()}` || `No record`,
+      'Taxable Status': `${parcel.taxable_status}` || `Unknown`,
       'Taxable Value': `$${_.parseInt(parcel.taxable_value).toLocaleString()}` || `No record`,
-      'Dimensions': `${parcel.depth} x ${parcel.frontage}`,
+      'Land Value': `$${_.parseInt(parcel.land_value).toLocaleString()}` || `No record`,
+      'Improved Value': `$${_.parseInt(parcel.improved_value).toLocaleString()}` || `No record`,
+      'Depth x Frontage': `${parcel.depth} x ${parcel.frontage} ft`,
+      'Total Acres': `${parcel.total_acres}`,
       'Zoning': parcel.zoning,
       '# of Buildings': parcel.num_buildings,
       'Legal Description': parcel.legaldesc,
     }
+
     return (
-      (
-        <div>
+      <div>
         <table>
-          {Object.keys(data).map(k => (
-            <tr>
-              <td className="fw7 w4 pv1" style={{borderBottom: '1px dashed #ddd', alignItems: 'top'}}>{k}</td>
-              <td className="pl2 fw4 h2" style={{borderBottom: '1px dashed #ddd'}}>{data[k]}</td>
-            </tr>
-          ))}
+          <tbody>
+            {Object.keys(data).map(k => (
+              <tr key={k}>
+                <td className="fw7 w4 pv1" style={{borderBottom: '1px dashed #ddd', alignItems: 'top'}}>{k}</td>
+                <td className="pl2 fw4 h2" style={{borderBottom: '1px dashed #ddd'}}>{data[k]}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        </div>
-      )
-    )
+      </div>
+    );
   }
 }
 
