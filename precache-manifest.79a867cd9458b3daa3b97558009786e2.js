53b9a04fe75b1a37c7d12ce9e418e354b2d9be6a
Jimmy McBroom <jimmy@onyx.local>
2020-10-31 12:42:51 -0400

    Updates

---
+++ precache-manifest.79a867cd9458b3daa3b97558009786e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59b97cd0f1e2725cacaf3f65756f396",
+    "url": "/parcel-viewer/index.html"
+  },
+  {
+    "revision": "277da2c5d683862ae5ab",
+    "url": "/parcel-viewer/static/css/main.f7376b33.chunk.css"
+  },
+  {
+    "revision": "39587630b6ed49c7f5ca",
+    "url": "/parcel-viewer/static/js/2.ef7fc0eb.chunk.js"
+  },
+  {
+    "revision": "277da2c5d683862ae5ab",
+    "url": "/parcel-viewer/static/js/main.d9e70344.chunk.js"
+  },
+  {
+    "revision": "0b9553921c47b356dbf4",
+    "url": "/parcel-viewer/static/js/runtime~main.853d3f96.js"
+  },
+  {
+    "revision": "c5600d7e84d66708778d9ac07c194f6e",
+    "url": "/parcel-viewer/static/media/logo.c5600d7e.png"
+  }
+]);
