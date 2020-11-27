7ff3b46dceef5da430fe1e1429ccd4a7191f1464
Jimmy McBroom <jimmy@onyx.local>
2020-11-27 01:19:40 -0500

    edit introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,4 +1,4 @@
-import { faCalendarAlt, faMapMarked, faStreetView, faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarAlt, faGlobe, faMapMarked, faStreetView, faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -41,20 +41,15 @@ const Introduction = ({ setInfo, showInfo }) => {
       </IntroPara>
       <ul className="list-disc list-inside ml-2 py-2 text-sm">
         <li>
-          On a phone or tablet, tap the icon to switch between <b>map</b>
-          <FontAwesomeIcon icon={faMapMarked} className="mx-2" />
-          or{" "}
-          <b>
-            street view
-            <FontAwesomeIcon icon={faStreetView} className="mx-2" />
-          </b>
-        </li>
-        <li>
           Switch between different imagery <b>capture dates</b>
           <FontAwesomeIcon icon={faCalendarAlt} className="mx-2" />
           using the date picker
         </li>
       </ul>
+      <IntroPara>
+        You can also switch the map baselayer to <b>satellite imagery</b>
+        <FontAwesomeIcon icon={faGlobe} className="mx-2" />
+      </IntroPara>
       <IntroPara>Do you have questions or comments about this tool? Tell us using our <a href="https://airtable.com/shrInZsfyRoJvq8GJ" className="underline text-gray-700 hover:text-blue-600">feedback form</a> or please open a <a href="https://github.com/CityOfDetroit/parcel-viewer/issues" className="underline text-gray-700 hover:text-blue-600">GitHub issue</a>.</IntroPara>
     </section>
   );
