ef72799d3f1e3947f7edbaadfc01f7e90f7768bf
Jimmy McBroom <james.mcbroom@gmail.com>
2025-12-11 15:05:32 -0500

    Updates

---
+++ precache-manifest.83b087800e2897e45ba36323d685b27b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9188e857f02a2de4c600e5c808d0ecff",
+    "url": "/parcel-viewer/index.html"
+  },
+  {
+    "revision": "2a9390fea9c678d67d3c",
+    "url": "/parcel-viewer/static/css/2.0d5a3f35.chunk.css"
+  },
+  {
+    "revision": "4bce42a2ac8b97454eaa",
+    "url": "/parcel-viewer/static/css/main.4cfa1874.chunk.css"
+  },
+  {
+    "revision": "2a9390fea9c678d67d3c",
+    "url": "/parcel-viewer/static/js/2.d95e9c12.chunk.js"
+  },
+  {
+    "revision": "4bce42a2ac8b97454eaa",
+    "url": "/parcel-viewer/static/js/main.3e8ccbb0.chunk.js"
+  },
+  {
+    "revision": "a916e60ae8f3d35a0ab2",
+    "url": "/parcel-viewer/static/js/runtime~main.4cefa8b9.js"
+  },
+  {
+    "revision": "c5600d7e84d66708778d9ac07c194f6e",
+    "url": "/parcel-viewer/static/media/logo.c5600d7e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/parcel-viewer/precache-manifest.51f6deef621b32f2c5f244842cd5e8c7.js"
+  "/parcel-viewer/precache-manifest.83b087800e2897e45ba36323d685b27b.js"
 );
 
 self.addEventListener('message', (event) => {
